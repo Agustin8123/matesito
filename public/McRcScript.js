e624fa8dfc827b2ff3e8ee7319b8975f27cdecfe
Agustín Couette <couetteagusQgmail.com>
2025-01-14 21:39:41 -0300

    modified:   public/McRcScript.js

--- public/McRcScript.js
+++ public/McRcScript.js
@@ -39,38 +39,42 @@ reactions.forEach(async function (reaction) {
 
   el.addEventListener("click", function (evt) {
     fetch(
-      `https://${API_BASE}/hit/microreact--reactions/${encodeURIComponent(
-        id
-      )}-${reaction}`
+      `https://${API_BASE}/hit/microreact--reactions/${encodeURIComponent(id)}/${reaction}`,
+      {
+        method: "POST", // Cambiamos el método a POST
+        headers: {
+          "Content-Type": "application/json", // Configuración opcional si envías datos
+        },
+      }
     );
     el.style.opacity = "0";
     el.style.transform = "scale(0.8) rotate(20deg)";
     list.style.opacity = "0";
     list.style.marginBottom = "-10px";
-
+  
     let originalText = el.innerText;
-
+  
     setTimeout(async function () {
       list.style.opacity = "1";
       list.style.marginBottom = "0px";
       el.style.opacity = ".7";
       el.innerText = "✔️";
       el.style.transform = "scale(1)";
-
+  
       list.innerText = parseInt(list.innerText) + 1;
     }, 250);
-
+  
     setTimeout(function () {
       el.style.opacity = "0";
       el.style.transform = "scale(0.8) rotate(60deg)";
     }, 1250);
-
+  
     setTimeout(function () {
       el.style.opacity = "1";
       el.style.transform = "scale(1)";
       el.innerText = originalText;
     }, 1500);
-
+  
     if (!allowMultiple) {
       document.querySelectorAll(`[data-reaction-id]`).forEach((l) => {
         l.setAttribute("disabled", "true");
