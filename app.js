9ed19c2e579684d60507e9dcd17e63001e32b8c8
Agustín Couette <couetteagusQgmail.com>
2025-02-20 23:17:31 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -1278,7 +1278,7 @@ app.get('/notificaciones/:user_id', async (req, res) => {
                         [noti.chat_or_group_id]
                     );
                     if (grupo.rows.length > 0) nombre = grupo.rows[0].name;
-                } else if (noti.tipo === 'chat') {             
+                } else if (noti.tipo === 'mensaje') {             
                     const sender = await db.query(
                         `SELECT username FROM users WHERE id = $1`,
                         [noti.chat_or_group_id]
