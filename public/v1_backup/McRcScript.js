6426c6763d9100dbae140c7eb4c74383af791f53
FeFe <90733495+squaredfe@users.noreply.github.com>
2025-11-02 00:53:14 -0300

    testing-v2-temp

---
+++ public/v1_backup/McRcScript.js
@@ -0,0 +1,163 @@
+// Función para obtener el userID desde las cookies
+function getUserIDFromCookies() {
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+      const [name, value] = cookie.split("=");
+      if (name === "userID") {
+          return decodeURIComponent(value);
+      }
+  }
+  return null;
+}
+
+const userID = getUserIDFromCookies();
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const reactions = (params.get("reactions") || "12345").split("");
+const allowMultiple = !!(params.get("allowMultiple") || 0);
+const textColor = decodeURIComponent(params.get("textColor") || "") || false;
+const bgColor = decodeURIComponent(params.get("bgColor") || "") || false;
+const font = decodeURIComponent(params.get("font") || "") || false;
+
+const API_BASE =
+decodeURIComponent(params.get("api_base") || "") || "matesito.com.ar";
+
+if (!id) {
+  document.body.innerHTML = `<div style="background-color:red;color:white;font-size:11.6px;padding:3px;font-family:monospace;overflow: auto;width: 262px;height: 80px;"><b>MicroReact ERROR [for website owner]:</b> No unique identifier set in URL parameters. Check <a href="microreact.glitch.me" style="color:white;font-weight: bold;">the docs</a> for usage.<br><br>Please check your MicroReact iframe's URL and verify that it has the URL parameter "id"</div>`;
+  throw new Error(
+      "MicroReact ERROR: No unique identifier set in URL parameters. More details inside the iframe."
+  );
+}
+
+reactions.forEach(async function (reaction) {
+  const el = document.querySelector(`[data-reaction-id="${reaction}"]`);
+  const list = document.querySelector(`[data-list-id="${reaction}"]`);
+
+  if (!el || !list) {
+      document.body.innerHTML = `<div style="background-color:red;color:white;font-size:11.6px;padding:3px;font-family:monospace;overflow: auto;width: 262px;height: 80px;"><b>MicroReact ERROR [for website owner]:</b> Reaction ID ${reaction
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")} not found. Check <a href="microreact.glitch.me" style="color:white;font-weight: bold;">the docs</a> for usage.<br><br>Please check your MicroReact iframe's URL and verify that its reaction IDs are only 1, 2, 3, 4, or 5. Example: "15". Default: "12345"</div>`;
+      throw new Error("MicroReact ERROR: Reaction ID does not exist. More details inside the iframe.");
+      return;
+  }
+
+  el.style.display = "block";
+  list.style.display = "block";
+
+  el.addEventListener("click", function () {
+      if (!userID) {
+          alert("Error: No se encontró un ID de usuario en las cookies.");
+          return;
+      }
+
+      fetch(`https://${API_BASE}/hit/microreact--reactions/${encodeURIComponent(id)}/${reaction}`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ user_id: userID }),
+      })
+      .then(response => {
+          el.style.opacity = "0";
+          el.style.transform = "scale(0.8) rotate(20deg)";
+          list.style.opacity = "0";
+          list.style.marginBottom = "-10px";
+
+          let originalText = el.innerText;
+
+          setTimeout(async function () {
+              list.style.opacity = "1";
+              list.style.marginBottom = "0px";
+              el.style.opacity = ".7";
+              el.innerText = "✔️";
+              el.style.transform = "scale(1)";
+
+              try {
+                  const reactionData = await fetch(
+                      `https://${API_BASE}/get/microreact--reactions/${encodeURIComponent(id)}?reaction=${encodeURIComponent(reaction)}`
+                  );
+                  const json = await reactionData.json();
+                  list.innerText = json.value || 0;
+              } catch (error) {
+                  console.error("Error al obtener las reacciones:", error);
+                  list.innerText = "0";
+              }
+          }, 250);
+
+          setTimeout(function () {
+              el.style.opacity = "0";
+              el.style.transform = "scale(0.8) rotate(60deg)";
+          }, 1250);
+
+          setTimeout(function () {
+              el.style.opacity = "1";
+              el.style.transform = "scale(1)";
+              el.innerText = originalText;
+          }, 1500);
+
+          if (!allowMultiple) {
+              document.querySelectorAll(`[data-reaction-id]`).forEach((l) => {
+                  l.setAttribute("disabled", "true");
+              });
+              document.querySelectorAll(`[data-list-id]`).forEach((l) => {
+                  if (!(l === list)) {
+                      l.style.opacity = ".4";
+                  }
+              });
+          }
+      })
+      .catch(error => {
+          console.error("Error al realizar la solicitud de reacción:", error);
+      });
+  });
+
+  try {
+      const reactionData = await fetch(
+        `https://${API_BASE}/get/microreact--reactions/${encodeURIComponent(id)}?reaction=${encodeURIComponent(reaction)}`
+      );
+      const json = await reactionData.json();
+      list.innerText = json.value || 0;
+  } catch {
+      list.innerText = "0";
+  }
+
+  socket.on('reloadReactions', async (data) => {
+    const { id } = data;
+    try {
+        const reactionData = await fetch(
+            `https://${API_BASE}/get/microreact--reactionss/${encodeURIComponent(id)}`
+        );
+        const json = await reactionData.json();
+
+        // Iterar sobre todas las reacciones para actualizarlas
+        if (json.reactions && json.reactions.length > 0) {
+            json.reactions.forEach((reaction) => {
+                const listItem = document.querySelector(`#reaction-${reaction.reaction_id}`);
+                if (listItem) {
+                    listItem.innerText = reaction.count;
+                }
+            });
+        }
+    } catch {
+        console.error("Failed to load reaction counts");
+    }
+});
+});
+
+let css = "";
+
+if (textColor) {
+  css = css + `* {color: ${textColor} !important}` + "\n";
+}
+if (bgColor) {
+  css = css + `body {background-color: ${bgColor} !important}` + "\n";
+}
+if (font) {
+  css = css + `* {font-family: ${font} !important}` + "\n";
+}
+
+const styleEl = document.createElement("style");
+styleEl.textContent = css;
+document.body.appendChild(styleEl);
---
+++ public/v1_backup/scripts2.js
@@ -0,0 +1,185 @@
+// Función genérica para alternar la visibilidad de un element
+// Menú desplegable principal
+  function toggleMenu() {
+    hideMenus('SearchMenu', 'rightMenu', 'forumSubMenu', 'userSubMenu');
+    toggleVisibility('dropdownMenu');
+}
+
+  function togglePosts() {
+    const profileList = document.getElementById("profileList");
+    const unicPostList = document.getElementById("unicPostList");
+    const forumList = document.getElementById("forumList");
+    const messageList = document.getElementById("messageList");
+    const groupMessageList = document.getElementById("groupMessageList");
+
+    const postList = document.getElementById('postList');
+
+    profileList.style.display = 'none';
+    unicPostList.style.display = 'none';
+    forumList.style.display = 'none';
+    messageList.style.display = 'none';
+    groupMessageList.style.display = 'none';
+    postList.style.display = 'block';
+}
+
+
+
+// Menú lateral derecho
+  function toggleRightMenu() {
+    hideMenus('SearchMenu', 'dropdownMenu');
+    toggleVisibility('rightMenu');
+}
+
+// Menú de búsqueda
+  function toggleSearch() {
+    hideMenus('rightMenu', 'dropdownMenu');
+    toggleVisibility('SearchMenu');
+}
+
+// Submenús específicos
+  function toggleForumMenu() {
+    toggleVisibility('forumSubMenu');
+}
+
+  function toggleMyForumMenu() {
+    toggleVisibility('myForumMenu');
+}
+
+  function toggleGruposMenu() {
+    toggleVisibility('gruposMenu');
+}
+
+  function userJoinedGroupMenu() {
+    toggleVisibility('userJoinedMenu');
+}
+
+  function usercreatedGroupMenu() {
+    toggleVisibility('userCreatedMenu');
+}
+
+  function toggleUserMenu() {
+    toggleVisibility('userSubMenu');
+    loadFollowedUsers(); 
+}
+
+  function createForumMenu() {
+    toggleVisibility('createForumOverlay', 'flex');
+    toggleRightMenu();
+}
+
+  function createGroupMenu() {
+    toggleVisibility('createGroupOverlay', 'flex');
+    toggleRightMenu();
+}
+
+  function toggleBell() {
+    toggleVisibility('notifMenu', 'flex');
+    obtenerNotificaciones();
+  }
+
+  function joinGroupMenu() {
+    toggleVisibility('joinGrupoMenu', 'flex');
+    toggleRightMenu();
+}
+
+  function exploreForumMenu() {
+    toggleVisibility('exploreforumMenu');
+    loadForos();
+}
+
+  function exploreForumMenu2() {
+    toggleVisibility('exploreforumMenu2');
+    loadUserForums();
+}
+
+// Otros submenús
+  function toggleSubMenu() {
+    toggleVisibility('subMenu');
+}
+
+  function toggleSubMenu2() {
+    toggleVisibility('subMenu2');
+}
+
+  function toggleDetails(menu, groupId) {
+    toggleVisibility(menu);
+    fetch(` /grupo/${groupId}`)
+        .then(response => response.json())
+        .then(group => {
+            // Seleccionar el contenedor dinámico basado en el groupId
+            const detailsElement = document.getElementById(`GroupDetailsContainer-${groupId}`);
+            if (detailsElement) {
+                detailsElement.innerHTML = `
+                    <h3>${group.name}</h3>
+                    <p>${group.description}</p>
+                    <p>Código de invitación: ${group.invite_code}</p>
+                `;
+            } else {
+                console.error(`No se encontró el contenedor para los detalles del grupo con ID: ${groupId}`);
+            }
+        })
+        .catch(error => console.error('Error al cargar los detalles del grupo:', error));
+}
+
+  function toggleDetails1(menu, groupId) {
+    toggleVisibility(menu);
+    fetch(` /grupo/${groupId}`)
+        .then(response => response.json())
+        .then(group => {
+            // Seleccionar el contenedor dinámico basado en el groupId
+            const detailsElement = document.getElementById(`groupDetailsContainer-${groupId}`);
+            if (detailsElement) {
+                detailsElement.innerHTML = `
+                    <h3>${group.name}</h3>
+                    <p>${group.description}</p>
+                    <p>Código de invitación: ${group.invite_code}</p>
+                `;
+            } else {
+                console.error(`No se encontró el contenedor para los detalles del grupo con ID: ${groupId}`);
+            }
+        })
+        .catch(error => console.error('Error al cargar los detalles del grupo:', error));
+}
+
+  function toggle_UserMenu(user) {
+    toggleVisibility(user);
+
+}
+
+  function toggle_ForumMenu(forum) {
+    toggleVisibility(forum);
+}
+
+  function toggle_GroupMenu(groupId) {
+    const menu = document.getElementById(`${groupId}`);
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+  function toggle_GroupMenu1(groupId) {
+    const menu = document.getElementById(`${groupId}`);
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+function openFullscreen(element) {
+  if (element.requestFullscreen) {
+      element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) { /* Safari */
+      element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) { /* IE11 */
+      element.msRequestFullscreen();
+  }
+}
+
+// Overlay inicial
+  function Acept() {
+    document.getElementById('initialOverlay').style.display = 'flex';
+    document.getElementById('AvisoOverlay').style.display = 'none';
+}
